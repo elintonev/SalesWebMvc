09ecd4f4c444a5736e2dedcd70ef5b8ee1c47e88
Elinton Henrique Ev <elintonev@hotmail.com>
2021-10-04 19:18:56 -0300

    SellerService and basic FindAll

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,20 @@ using System.Threading.Tasks;
 
 namespace SalesWebMvc.Controllers
 {
-    public class SellersController : Controller
+    public class SellersController : Controller //Recebeu a chamada do caminho /Sellers que apresenta no navegador
     {
-        public IActionResult Index()
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
         {
-            return View();
+            _sellerService = sellerService;
+        }
+
+        public IActionResult Index()  //Como não tinba nenhuma ação configurada, a ação Index foi acionada.
+                                      //O IActionResult chamou a View(), e a VIEW foi na pasta Views.Sellers e buscou pela view INDEX
+        {
+            var list = _sellerService.FindAll();
+            return View(list);
         }
 
     }
