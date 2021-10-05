9df5f19c91680ab099fb8406f2927c341e065afb
Elinton Henrique Ev <elintonev@hotmail.com>
2021-10-05 01:20:06 -0300

    Simple Create form

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,14 @@ namespace SalesWebMvc.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
-
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost] //Notação para definir que o método abaixo seja lido como um HTTPPOST
+        [ValidateAntiForgeryToken] //Notação para definit que os dados pessoais do formulário não possam ser usados por token maliciosos
+        public IActionResult Create(Seller seller) {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -18,5 +18,9 @@ namespace SalesWebMvc.Services
         public List<Seller> FindAll() {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj) {
+            _context.Add(obj); //Metodo que pega minha variável do tipo Seller e insere no banco de dados
+            _context.SaveChanges(); // Método que confirma a ação de inserir informações no banco de dados
+        }
     }
 }
