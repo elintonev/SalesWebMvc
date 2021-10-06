7ba84c5b57fe806e72ce3d19a56e36c5e87e64fd
Elinton Henrique Ev <elintonev@hotmail.com>
2021-10-06 11:23:35 -0300

    Seller details and eager loading

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -23,12 +24,13 @@ namespace SalesWebMvc.Services
             _context.SaveChanges(); // Método que confirma a ação de inserir informações no banco de dados
         }
         public Seller FindById(int id) {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id) {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+
     }
 }
