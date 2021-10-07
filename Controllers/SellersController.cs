5aacca6fee2a5156a47288daff8e52aa072db0cd
Elinton Henrique Ev <elintonev@hotmail.com>
2021-10-07 14:03:05 -0300

    Asynchronous operations using Tasks (async, await)

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -22,34 +22,34 @@ namespace SalesWebMvc.Controllers
             _departmentService = departmentService;
         }
 
-        public IActionResult Index()  //Como não tinba nenhuma ação configurada, a ação Index foi acionada.
+        public async Task<IActionResult> Index()  //Como não tinba nenhuma ação configurada, a ação Index foi acionada.
                                       //O IActionResult chamou a View(), e a VIEW foi na pasta Views.Sellers e buscou pela view INDEX
         {
-            var list = _sellerService.FindAll();
+            var list =  await _sellerService.FindAllAsync();
             return View(list);
         }
-        public IActionResult Create() {
-            var departments = _departmentService.FindAll();
+        public async Task<IActionResult> Create() {
+            var departments = await _departmentService.FindAllAsync();
             var viewModels = new SellerFormViewModel { Departments = departments};
             return View(viewModels);
         }
         [HttpPost] //Notação para definir que o método abaixo seja lido como um HTTPPOST
         [ValidateAntiForgeryToken] //Notação para definit que os dados pessoais do formulário não possam ser usados por token maliciosos
-        public IActionResult Create(Seller seller) {
+        public async Task<IActionResult> Create(Seller seller) {
             if (!ModelState.IsValid)
             {
-                var departments = _departmentService.FindAll();
+                var departments = await _departmentService.FindAllAsync();
                 var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
                 return View(viewModel);
             }
-            _sellerService.Insert(seller);
+            await _sellerService.InsertAsync(seller);
             return RedirectToAction(nameof(Index));
         }
-        public IActionResult Delete(int? id) {
+        public async Task<IActionResult> Delete(int? id) {
             if (id == null) {
                 return RedirectToAction(nameof(Error), new { message = "Id not Provided" });
             }
-            var obj = _sellerService.FindById(id.Value);
+            var obj = await _sellerService.FindByIdAsync(id.Value);
             if (obj == null) {
                 return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
@@ -57,47 +57,53 @@ namespace SalesWebMvc.Controllers
         }
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult Delete(int id) {
-            _sellerService.Remove(id);
+        public async Task<IActionResult> Delete(int id) {
+            await _sellerService.RemoveAsync(id);
             return RedirectToAction(nameof(Index));
         }
-        public IActionResult Details(int? id)
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
-            var obj = _sellerService.FindById(id.Value);
+            var obj = await _sellerService.FindByIdAsync(id.Value);
             if (obj == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
-        public IActionResult Edit(int? id)
+        public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
-            var obj = _sellerService.FindById(id.Value);
+            var obj = await _sellerService.FindByIdAsync(id.Value);
             if (obj == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
-            List<Department> departments = _departmentService.FindAll();
+            List<Department> departments = await _departmentService.FindAllAsync();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
             return View(viewModel);
         }
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public IActionResult Edit(int? id, Seller seller) {
+        public async Task<IActionResult> Edit(int? id, Seller seller) {
+            if (!ModelState.IsValid)
+            {
+                var departments = await _departmentService.FindAllAsync();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             if (id != seller.Id) {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
             try
             {
-                _sellerService.update(seller);
+                await _sellerService.update(seller);
                 return RedirectToAction(nameof(Index));
             }
             catch (ApplicationException e) {
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -17,29 +17,30 @@ namespace SalesWebMvc.Services
         {
             _context = context;
         }
-        public List<Seller> FindAll() {
-            return _context.Seller.ToList();
+        public async Task<List<Seller>> FindAllAsync() {
+            return await _context.Seller.ToListAsync();
         }
-        public void Insert(Seller obj) {
+        public async Task InsertAsync(Seller obj) {
             _context.Add(obj); //Metodo que pega minha variável do tipo Seller e insere no banco de dados
-            _context.SaveChanges(); // Método que confirma a ação de inserir informações no banco de dados
+            await _context.SaveChangesAsync(); // Método que confirma a ação de inserir informações no banco de dados
         }
-        public Seller FindById(int id) {
-            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        public async Task<Seller> FindByIdAsync(int id) {
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
         }
-        public void Remove(int id) {
-            var obj = _context.Seller.Find(id);
+        public async Task RemoveAsync(int id) {
+            var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
-        public void update(Seller obj) {
-            if (!_context.Seller.Any(x => x.Id == obj.Id)) {
+        public async Task update(Seller obj) {
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny) {
                 throw new NotFoundException("Id not found");
             }
             try
             {
                 _context.Update(obj);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException e) {
                 throw new DbConcurrencyException(e.Message);
